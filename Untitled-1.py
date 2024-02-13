6fdd27015fade4f9f36bc20b26169a39ed68573a
wvwysee <dibiloid1928@gmail.com>
2024-02-13 20:37:05 +0200

    start1

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,49 +2,19 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot('6548496739:AAGrzcsKDKzqV-1uLRMUVvuLwLieoa5auv0')
-
 @bot.message_handler(commands=['start'])
+def ques(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    
 
-def start(message):
+    whatbotcando = types.InlineKeyboardMarkup('whatbotcando', callback_data='You can click ob button "getnews" and enter 1 keyword, then "NewsBOT" will respond with 10 lastest news and links to them!')
+    aboutcreator = types.InlineKeyboardMarkup('aboutcreator', callback_data='Person that created me is known as "Bilousov Oleksandr')
+    getnews = types.InlineKeyboardMarkup('getnews', callback_data='Enter 1 keyword')
 
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('whatbotcando')
-    markup.row(btn1)
-    btn2 = types.KeyboardButton('whatbotcando')
-    markup.row(btn2)
-    btn3 = types.KeyboardButton('getnews ')
-    markup.row(btn3)
-    bot.send_message(message.chat.id, 'Привіт', reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    markup.add(whatbotcando,aboutcreator,getnews)
 
 
-
-    def on_click(message):
-        if message.text == 'whatbotcando':
-            bot.send_message(message.chat.id, 'You can click ob button "getnews" and enter 1 keyword, then "NewsBOT" will respond with 10 lastest news and links to them!')
-        elif message.text == 'abotcreator':
-            bot.send_message(message.chat.id, 'Person that created me is known as "Bilousov Oleksandr')
-        elif message.text == 'getnews':
-            bot.send_message(message.chat.id, 'Enter 1 keyword')
-            input(message)
-
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id,f'Привіт,{message.from_user.first_name}')
-
-    @bot.message_handler(commands=['help'])
-    def main(message):
-        bot.send_message(message.chat.id,'/start''\n' '/menu' '\n' '/help')
-
-        @bot.message_handler(commands=['menu'])
-        def main(message):
-            bot.send_message(message.chat.id, 'Головне меню')
-
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'привіт':
-        bot.send_message(message.chat.id,f'Привіт,{message.from_user.first_name}')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message,f'ID:{message.from_user.id}')
-
-bot.polling(none_stop=True)
+    bot.send_message(message.chat.id, 'What u want to know?', reply_markup=markup)
+#@bot.callback_query_handler(func=lambda call:True)
+#def answ(callb):
+    bot.polling()
