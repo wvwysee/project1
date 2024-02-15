76766f979e70fb850371dbba82f502c753c5b560
wvwysee <dibiloid1928@gmail.com>
2024-02-15 20:24:08 +0200

    p2

--- project.py
+++ project.py
@@ -1,44 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Функция для выполнения парсинга по заданному слову и вывода ссылок на статьи
-def parse_website(url, target_word, max_articles=10):
-    # Выполняем GET-запрос к веб-странице
-    response = requests.get(url)
-    
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        # Создаем объект BeautifulSoup для парсинга HTML
-        soup = BeautifulSoup(response.text, 'lxml')
+def pars(url,target_word, max_arc=10):
+    url=""
+    response=requests.get(url)
+    if response.status_code==200:
         
-        # Ищем все ссылки на странице
-        links = soup.find_all('a', href=True)
-        
-        # Счетчик найденных статей
-        articles_found = 0
-        
-        # Проходим по каждой ссылке и проверяем наличие заданного слова
-        for link in links:
-            article_url = link['href']
-            # Игнорируем ссылки без текста и ссылки на ту же страницу
-            if link.text.strip() and article_url != url:
-                article_response = requests.get(article_url)
-                if article_response.status_code == 200:
-                    article_soup = BeautifulSoup(article_response.text, 'html.parser')
-                    # Поиск заданного слова в тексте статьи
-                    if target_word in article_soup.get_text():
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+
+
+
+    links=soup.get_all('a', href=True)
+    artic_match = 0
+
+    for link in links:
+        arcticle_url =link['href']
+
+        if link.text.strip() and arcticle_url != url:
+            arcticle_response = requests.get(arcticle_url)
+            if arcticle_response.status_code == 200:
+                article_soup = BeautifulSoup(arcticle_response.text, 'html.parser')
+                if target_word in article_soup.get_text():
                         print(f"Статья: {link.text.strip()}")
-                        print(f"Ссылка: {article_url}")
+                        print(f"Ссылка: {arcticle_url}")
                         print()
-                        articles_found += 1
-                        # Выходим из цикла, если достигнуто максимальное количество статей
-                        if articles_found >= max_articles:
+                        artic_match += 1
+                        if artic_match >= max_arc:
                             break
     else:
         # В случае ошибки выводим сообщение
         print("Ошибка при выполнении запроса:", response.status_code)
 
-# Пример использования функции
-url = "hhttps://www.nbcnews.com/world"
-word_to_search = "fight"
-parse_website(url, word_to_search)
+
